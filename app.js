3491f2158f472c7c0f7166f8f7d3b4b49e30b49c
Houssam OUATMANI <ouatmanihoussam@gmail.com>
2021-02-10 21:56:55 +0100

    project init

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log('aze')
+const form = document.querySelector('form')
+const submit = document.querySelector('button')
+
+
+
+async function fetchData(){
+    const url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
+    const {value} = document.querySelector('input')
+    let data = await fetch(url+value)
+    let response = await data.json()
+    return response
+}
+
+function displayData(reponse){
+    reponse.map(meal => {
+        console.log(meal)
+    })
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    fetchData().then(reponse => {
+        displayData(reponse.meals)
+    })
+})
